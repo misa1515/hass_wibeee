6a5c9c86c104bb32ccb7fff3da0086fc898b4446
Luis Miranda <161006+luuuis@users.noreply.github.com>
2021-08-22 18:30:32 +0100

    fix: specify `iot_class` for custom component, add hassfest workflow (#1)
    * add hassfest GitHub workflow so we know when something is about to break
    * specify `iot_class` for custom component
    * move into custom_components dir to please hassfest (dir name vs domain)

--- __init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Support for Energy consumption Sensors from Circutor
-
-Device's website: http://wibeee.circutor.com/
-Documentation: https://github.com/juanjoSanz/hass_wibeee/
-"""
---
+++ custom_components/wibeee/__init__.py
@@ -0,0 +1,5 @@
+"""
+Support for Energy consumption Sensors from Circutor
+Device's website: http://wibeee.circutor.com/
+Documentation: https://github.com/juanjoSanz/hass_wibeee/
+"""
